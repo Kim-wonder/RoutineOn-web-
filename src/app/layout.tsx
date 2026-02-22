df120830ed047af07cf2380f471f18746b08bb86
heajin-kim <103446802+Kim-wonder@users.noreply.github.com>
2026-02-22 23:59:17 +0900

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "운동알람 - workOut_alarm",
+  title: "Routine_ON : 운동영상알림",
   description: "유튜브 운동 영상 알람 프로토타입",
 };
 
